f83ca99a244db97d9232c601279580378517baf7
Ezequiel Zink <ezequiel.zink@avature.net>
2022-09-20 23:03:02 -0300

    setting up basic public pwa

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
+var APP_PREFIX = 'zn-sleepbox_';
+var VERSION = 'version_001';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
