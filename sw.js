fe2ae6de1c3fe055c0365f30b6d139905e7d57f8
Ezequiel Zink <ezequiel.zink@gmail.com>
2022-09-23 11:51:28 -0300

    version changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/github-page-pwa';
 var APP_PREFIX = 'zn-sleepbox_';
-var VERSION = 'version_004';
+var VERSION = 'version_004a';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
